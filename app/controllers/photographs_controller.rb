cae480a269b3d2bbf08dbf4c22412fbb40e5f3b2
dagjwtpoad <dagjwtpoad@gmail.com>
2024-03-14 22:04:54 +0900

    編集機能 1st commit

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -1,5 +1,5 @@
 class PhotographsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :edit, :destroy, :new]
 
   def index
     @photographs = Photograph.all.order(created_at: :desc)
@@ -20,6 +20,28 @@ class PhotographsController < ApplicationController
 
   def show
     @photograph = Photograph.find(params[:id])
+  end
+
+  def edit
+    @photograph = Photograph.find(params[:id])
+    unless current_user.id == @photograph.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    photograph = Photograph.find(params[:id])
+    if photograph.update(photograph_params)
+      redirect_to photograph_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    photograph = Photograph.find(params[:id])
+    photograph.destroy
+    redirect_to root_path
   end
 
   private
